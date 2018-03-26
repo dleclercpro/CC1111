7e7639ab4f727e3919686d1ea7cfa421c653d583
mm22dl <david.leclerc@tu-dresden.de>
2018-03-26 21:35:16 +0200

    Encoding/decoding of packets completed.

--- echo.py
+++ echo.py
@@ -5,6 +5,7 @@
 import usb.core
 import usb.util
 import time
+import math
 
 
 
@@ -145,6 +146,22 @@ def decode(bytes):
 
 
 
+def format(packet, n = 2):
+
+    """
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        FORMAT
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+
+    # Compute number of character groups of n length
+    N = int(math.ceil(len(packet) / float(n)))
+
+    # Return said groups with spaces in between
+    return " ".join([packet[(n * i):(n * (i + 1))] for i in range(0, N)])
+
+
+
 def decode4x6(bytes):
 
     """
@@ -158,8 +175,8 @@ def decode4x6(bytes):
              "100110", "010110", "011010", "011001", "101010", "001011",
              "101100", "001101", "001110", "011100"]
 
-    # Initialize packet
-    packet = []
+    # Show bytes
+    #print "Decoding: " + str(bytes)
 
     # Convert bytes to bits
     bits = ["{:08b}".format(x) for x in bytes]
@@ -167,11 +184,20 @@ def decode4x6(bytes):
     # Join them all together
     bits = "".join(bits)
 
+    # Show bits
+    #print "Bits: " + bits
+
+    # Initialize packet
+    packet = []
+
     # Scan bits
     while bits:
 
         # Get 6-bits word and shorten rest of bits
         word, bits = bits[:6], bits[6:]
+
+        # Show word
+        #print word
 
         # End-of-packet
         if word == "000000":
@@ -182,8 +208,11 @@ def decode4x6(bytes):
         # Try converting
         try:
 
-            # Convert word using conversion table
-            word = hex(TABLE.index(word))[-1].upper()
+            # Convert word to hexadecimal value using conversion table
+            word = TABLE.index(word)
+
+            # Format it
+            word = hex(word)[-1].upper()
 
             # Store word
             packet.append(word)
@@ -191,11 +220,31 @@ def decode4x6(bytes):
         # If error
         except ValueError:
 
-            # Skip
-            print "Error while converting bytes."
+            # If bits within packet
+            if bits != "":
+
+                # Raise error
+                raise NotImplementedError("Unmatched bits before EOP (" +
+                                          "corrupted packet): " + word)
+
+            # If last bits
+            else:
+
+                # If they do not fit
+                if word != "0101":
+
+                    # Raise error
+                    raise NotImplementedError("Last bits do not correspond " +
+                                              "to expectation (0101): " + word)
 
     # Stringify packet
     packet = "".join(packet)
+
+    # Format it
+    packet = format(packet)
+
+    # Show packet
+    #print "Decoded packet: " + packet
 
     # Return packet
     return packet
@@ -208,6 +257,82 @@ def encode4x6(packet):
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         ENCODE4X6
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Encoding example: power down
+          A7: RF type (pump comms)
+          79 91 63: pump serial number
+          5D: op code
+          00: parameters
+          C6: CRC
+    """
+
+    # Define conversion table
+    TABLE = ["010101", "110001", "110010", "100011", "110100", "100101",
+             "100110", "010110", "011010", "011001", "101010", "001011",
+             "101100", "001101", "001110", "011100"]
+
+    # Show packet
+    #print "Encoding: " + packet
+
+    # Remove spaces
+    packet = packet.replace(" ", "")
+
+    # Initialize bits
+    bits = ""
+
+    # Convert every character to its series of bits
+    for p in packet:
+
+        # Use table to convert it
+        p = TABLE[int(p, 16)]
+
+        # Add new bits
+        bits += p
+
+    # Add mysterious last bits
+    bits += "0101"
+
+    # Get number of bits
+    n = len(bits)
+
+    # If number of bits not multiple of 8, encoding fails
+    if n % 8 != 0:
+
+        # Raise error
+        raise NotImplementedError("Impossible to encode number of bytes " +
+                                  "which isn't a multiple of 8: " + str(n))
+
+    # Show bits
+    #print "Bits: " + bits
+
+    # Initialize bytes
+    bytes = []
+
+    # Convert bits to bytes
+    while bits:
+
+        # Get byte and shorten rest of bits
+        byte, bits = bits[:8], bits[8:]
+
+        # Convert byte from binary to decimal value
+        byte = int(byte, 2)
+
+        # Store byte
+        bytes.append(byte)
+
+    # Show bytes
+    #print "Encoded bytes: " + str(bytes)
+
+    # Return them
+    return bytes
+
+
+
+def oldEncode4x6(string):
+
+    """
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        OLDENCODE4X6
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     """
 
     # Define conversion table
@@ -215,12 +340,31 @@ def encode4x6(packet):
              0b100110, 0b010110, 0b011010, 0b011001, 0b101010, 0b001011,
              0b101100, 0b001101, 0b001110, 0b011100]
 
-    # Initialize bits and bytes
+    # Initialize bits, bytes and packet
     bits = []
     bytes = []
+    packet = []
 
-    # Scan packet
-    for b in [ord(p) for p in packet]:
+    # Show input string
+    print "Encoding: " + string
+
+    # Remove spaces
+    string.replace(" ", "")
+
+    # Convert it to bytes
+    while string:
+
+        # Every two characters correspond to one hexadecimal value (or byte)
+        byte, string = int(string[:2], 16), string[2:]
+
+        # Add it to bytes
+        bytes.append(byte)
+
+    # Show bytes
+    print bytes
+
+    # Scan them
+    for b in bytes:
 
         # Try converting
         try:
@@ -235,25 +379,28 @@ def encode4x6(packet):
         except ValueError:
 
             # Skip
-            print "Error while converting packet."
+            print "Error while encoding bytes."
 
     # Join them all together and pad them with extra zeros
     bits = "".join(bits) + 12 * "0"
 
-    # Retrieve bytes
+    # Convert bytes
     while len(bits) >= 8:
 
         # Get byte and shorten rest of bits
         byte, bits = bits[:8], bits[8:]
 
-        # Convert byte to decimal value
+        # Convert byte from binary to decimal value
         byte = int(byte, 2)
 
         # Store byte
-        bytes.append(byte)
+        packet.append(byte)
 
-    # Return bytes
-    return bytes
+    # Show packet
+    print "Encoded packet: " + str(packet)
+
+    # Return it
+    return packet
 
 
 
@@ -419,11 +566,32 @@ def test4x6():
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     """
 
-    # Test encoding 4x6
-    print encode4x6("A77991635D00C6")
+    # Define tests
+    tests = {
+        "Power down": ["A7 79 91 63 5D 00 C6",
+            [169, 101, 153, 103, 25, 163, 148, 213, 85, 178, 101]],
+        "Model": ["A7 79 91 63 8D 00 C8",
+            [169, 101, 153, 103, 25, 163, 104, 213, 85, 177, 165]],
+        "Time": ["A7 79 91 63 70 00 55",
+            [169, 101, 153, 103, 25, 163, 89, 85, 85, 150, 85]],
+        "Firmware": ["A7 79 91 63 74 00 0D",
+            [169, 101, 153, 103, 25, 163, 91, 69, 85, 84, 213]]
+    }
 
-    # Test decoding 4x6
-    print decode4x6([169, 101, 153, 103, 25, 163, 148, 213, 85, 178, 101])
+    # Loop through tests
+    for test, values in tests.iteritems():
+
+        # Give test name
+        print "-- " + test + " --"
+
+        # Test encoding
+        print "Encoding: " + str(encode4x6(values[0]) == values[1])
+
+        # Test decoding
+        print "Decoding: " + str(decode4x6(values[1]) == values[0])
+
+        # Breathe
+        print
 
 
 
@@ -458,11 +626,11 @@ def test(EPs):
 
 
 
-def main():
+def init():
 
     """
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        MAIN
+        INIT
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     """
 
@@ -491,8 +659,24 @@ def main():
     EPs = {"IN": getEP(config, "IN", 0),
            "OUT": getEP(config, "OUT", 0)}
 
+    # Return EPs
+    return EPs
+
+
+
+def main():
+
+    """
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        MAIN
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+
+    # Get EPs
+    #EPs = init()
+
     # Test radio
-    radio(EPs, 1000)
+    #radio(EPs, 1000)
 
     # Test register
     #register(EPs)
@@ -504,7 +688,7 @@ def main():
     #test(EPs)
 
     # Test encoding
-    #test4x6()
+    test4x6()
 
 
 
